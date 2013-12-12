84ffe4a62fc70ffe234d0cbe3c00eb25d52a9993
Kai Lieth <git@radiant-cove-7007>
2013-12-11 19:05:34 -0500

    a7 final grading

--- run.js
+++ run.js
@@ -1,8 +1,6 @@
 var writer = {
-	//timer: null,
 	buffer: "",
 	print: function(str) {
-		//if (!timer) timer = setInterval(send,2000);
 		this.buffer += str + '\n';
 	},
 	send: function() {
@@ -12,6 +10,9 @@ var writer = {
 			$.post("/echo", {val:this.buffer});
 			this.buffer = "";
 		}
+	},
+	clear: function() {
+		this.buffer = "";
 	}
 }
 
@@ -21,27 +22,11 @@ function stop() {
 	clearInterval(testing);
 }
 
-
-/*
-$(function() {
-	$.get("/tests", function (data, text) {
-		var allNumbers = text.split(" ");
-		for (var i = 0; i < allNumbers.length; i++) {
-			allNumbers[i] = Number(allNumbers[i]);
-		}
-		$('div.myNumbers').innerHTML(text);
-	});
-
-	$.post("/tests", function (data, text) {
-		$('input[name=myNumbers]').innerHTML(text);
-	});
-});
-*/
-
 $(function() {
 	var index = 0;
 	var files = null;
 	var tests = null;
+	var name;
 	$.get("/tests", function (data) {
 		tests = data;
 		console.log("TEST DATA:");
@@ -57,44 +42,78 @@ $(function() {
 	});
 	var page = $('#page');
 	page.load(function() {
+		writer.print("GRADING: " + name);
 		page[0].contentWindow.alert = function(data) {
 			writer.print("ALERT used: " + data);
 			console.log(data);
 		};
-		for (var i = 0; i < tests.length; i++) {
-			for (var j = 0; j < tests[i].input.length; j++) {
-				writer.print("Testing: " + tests[i].input[j].val);
-				page.contents().find('[name=' + tests[i].input[j].key + ']').val(tests[i].input[j].val);
-			}
-			page.contents().find('[type=button]').click();
-			for (var j = 0; j < tests[i].output.length; j++) {
-				var out = tests[i].output[j];
-				var ret = page.contents().find('[name=' + out.key + ']').val();
-				if (out.val == "error") {
-					if (out.errmsg) {
-						ret = "Should be \"ERROR: " + out.errmsg + "\" :: " + ret;
-					} else {
-						ret = "Should be ERROR: " + ret;
-					}
-				} else if (ret.indexOf(out.val) > -1) {
-					ret = "CORRECT: " + ret;
-				}
-				console.log(ret);
-				writer.print(ret);
-			}
-			//var ret = page.contents().find('[name=outFieldA]').val();
+		// TODO - Make teamObjs generic (e.g.: contentWindow[tests.fieldName])
+		console.log("setting raw data");
+		page.contents().find('[name=' + tests.inputId + ']').val(tests.rawData);
+		// TODO - Make this button click a safer operation (guarantee it with an id?)
+		page.contents().find('[type=button]').click();
+		var testObj = page[0].contentWindow.teamObjs;
+		if (tests.data.length != testObj.length) {
+			writer.print("Wrong number of team members (found " + testObj.length + " expected " + tests.data.length + ")");
 		}
-		//writer.print(page.contents().find('[name=outField]').val());
+		var allCorrect = Array();
+		for (var i = 0; i < testObj.length; i++) {
+			var objCorrect = Array();
+			var oindex = 0;
+			for (var prop in testObj[i]) {
+				var val = testObj[i][prop];
+				if (typeof(val) === "function") {
+					var ret = val.call(testObj[i]);
+					if (ret == tests.data[i][prop]) {
+						objCorrect[oindex] = "c";
+					} else if (!ret) {
+						objCorrect[oindex] = "n";
+					} else {
+						objCorrect[oindex] = "w";
+					}
+				} else {
+					if (val == tests.data[i][prop]) {
+						objCorrect[oindex] = "c";
+					} else if (!val) {
+						objCorrect[oindex] = "n";
+					} else {
+						objCorrect[oindex] = "w";
+					}
+				}
+				oindex++;
+			}
+			allCorrect[i] = (function(arr) {
+				var obj = {c:0,w:0,n:0};
+				for (var item = 0; item < arr.length; item++) {
+					obj[arr[item]]++;
+				}
+				return obj;
+			})(objCorrect);
+		}
+		console.log(allCorrect);
+		writer.print(JSON.stringify((function(arr) {
+			var obj = {c:0,w:0,n:0};
+			for (var item = 0; item < arr.length; item++) {
+				obj.c += arr[item].c;
+				obj.w += arr[item].w;
+				obj.n += arr[item].n;
+			}
+			return obj;
+		})(allCorrect)));
 		writer.send();
 	});
 	function kickoff() {
+		writer.clear();
 		console.log("Loading: " + files[index]);
-		writer.print("GRADING: " + files[index]);
+		name = files[index];
 		var newUrl = 'http://localhost:3131/sakai/' + escape(files[index]);
 		console.log('requesting: ' + newUrl);
 		page.attr('src',newUrl);
 		index++;
-		if (index >= files.length) clearInterval(testing);
+		if (index >= files.length) {
+			console.log("FINISHED");
+			clearInterval(testing);
+		}
 	}
 	testing = setInterval(kickoff,1500);
 });
